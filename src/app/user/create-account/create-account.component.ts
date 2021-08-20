519f28f5a8fc36ad7655434bdbac5f4f827ad9b3
AnggieCorrea <anggie.correa@javeriana.edu.co>
2021-08-20 16:35:47 -0500

    Avances en home, login, y createAccount

--- src/app/user/create-account/create-account.component.ts
+++ src/app/user/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+  
+  login():void{
+    this.router.navigate(['/login'])
+  }
+
+  createAccount():void{
+    this.router.navigate(['/createAccount'])
   }
 
 }

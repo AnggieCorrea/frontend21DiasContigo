be75bd51762d38965f6e1f53097f90c688edf6ed
Brayan-Gonzalez-1 <bjgonzaleza@gmail.com>
2021-08-26 10:07:22 -0500

    Cambios de Navbar

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -3,27 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  showContemplation = true;
+  showPause = false;
 
-  showContemplation=true;
-  showPause=false;
+  constructor() {}
 
-  constructor() {
-   }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  showContemplations(){
+  showContemplations() {
     this.showContemplation = true;
     this.showPause = false;
   }
 
-  showPauses(){
+  showPauses() {
     this.showContemplation = false;
     this.showPause = true;
   }
-
 }

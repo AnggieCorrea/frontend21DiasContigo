7ec6a9989c72ccbc1fa0ad3a7fa73f0024af3d4d
santiago jaramillo <luisjaramilloespinos16@gmail.com>
2021-11-07 17:49:57 -0500

    formatio basico de envio de mensajes

--- backend/messagestomail.py
+++ backend/messagestomail.py
@@ -1,13 +1,16 @@
+import os
 import smtplib
-from decouple import config 
+from email.message import EmailMessage
+'''EMAIL_ADDRESS =os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD =os.environ.get('EMAIL_PASS')'''
 
-def send_message(msg,mail):
-    message=msg
-    subject='correo de prueba '
-    message='Subject: {}\n\n {}'.format(subject,message)
-    server=smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    server.login('luisjaramilloespinos16@gmail.com', '')
-    server.sendmail('luisjaramilloespinos16@gmail.com','santidragon@hotmail.com',message)
-    server.quit()
-    print ('correo enviado exitosamente')
+with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login('luisjaramilloespinos16@gmail.com','dragon1998')
+    subject='notification about  register in 21 dias'
+    body='welcome to our new platfrom we are greatfull to count with you as new user'
+    msg=f'Subject:{subject}\n\n{body}'
+    smtp.sendmail('luisjaramilloespinos16@gmail.com','luisjaramilloespinos16@gmail.com',msg)
